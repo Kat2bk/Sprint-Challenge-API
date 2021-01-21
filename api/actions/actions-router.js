fe9ce4d61949699b77dbb0f57112737b653e000f
unknown <Katherine2bk@gmail.com>
2021-01-21 12:06:03 -0800

    there seems to be a process with knex and sqlite3... particularly around python... tried installing windows-build-tools and python... it seemed to help a little, but localhost has crashed and will no longer run the server... I'm attempting to try this project with another clean clone

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,19 @@
 // Write your "actions" router here!
+const express = require('express');
+const Actions = require('./actions-model');
+const router = express.Router();
+
+router.get('/:id', async (req, res) => {
+    const id = req.params.id;
+    try {
+        const data = await Actions.get(id);
+        res.status(200).json(data)
+    } catch (error) {
+        console.log(error)
+        next(error)
+    }
+})
+
+
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const helmet = require('helmet');
+const actionsRouter = require('./actions/actions-router');
 const errorMiddleWare = require('./errorMiddleware');
 const server = express();
 server.use(express.json())
 
 server.use(helmet());
+server.use('/api/actions', actionsRouter);
 
 server.get('/', (req, res) => {
     res.send('Welcome to the API')
