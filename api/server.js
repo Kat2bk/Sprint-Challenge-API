153e112cafd30542e62797dd33586e66aecbb74c
unknown <Katherine2bk@gmail.com>
2021-01-21 08:26:19 -0800

    for some reason dotenv will not install... API works now, was working earlier with it. either way, will begin working on routers

--- api/server.js
+++ api/server.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const helmet = require('helmet');
+const errorMiddleWare = require('./errorMiddleware');
 const server = express();
 server.use(express.json())
+
+server.use(helmet());
+
+server.get('/', (req, res) => {
+    res.send('Welcome to the API')
+})
+
+server.use(errorMiddleWare)
+
 
 // Complete your server here!
 // Do NOT `server.listen()` inside this file!
